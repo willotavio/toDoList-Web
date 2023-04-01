4fcbc1cc9ec2559b09024c9331b6a6191db8f44f
willotavio <willaotavio@gmail.com>
2023-04-01 14:34:35 -0300

    Creating node server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,
+    'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html');
+});
+
+app.listen(8081, '192.168.1.102' || 'localhost', function(){
+    console.log("Connected!");
+});
